e0ba1b9527060857a145a0fc64053e5c4f50c02f
davide della casa venturelli <davide.dellacasaventurelli@gmail.com>
2026-05-13 20:32:50 +0200

    Add slurm job id to wandb.

--- src/incremental_ad/core/tracking.py
+++ src/incremental_ad/core/tracking.py
@@ -5,12 +5,18 @@ import wandb
 
 
 def init_wandb(
-    *, # forces the next arguments to be provided by keyword and not by position.
+    *,  # forces the next arguments to be provided by keyword and not by position.
     group: str,
     job_type: str,
     name: str,
     config: dict[str, Any],
 ) -> None:
+
+    # Adds the slurm id explicitly to wandb if available.
+    slurm_job_id = os.environ.get("SLURM_JOB_ID")
+
+    if slurm_job_id is not None:
+        config = {**config, "slurm_job_id": slurm_job_id}
 
     wandb.init(
         project=os.environ.get("WANDB_PROJECT"),

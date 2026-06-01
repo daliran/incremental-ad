b5b54d83fa6b1334311592ab6990b79519ca9553
Davide Della Casa Venturelli <71622@studenti.unimore.it>
2026-06-02 00:11:46 +0200

    Fix cluster debug

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 PARTITION="all_serial"
-QOS="all_serial"
 ACCOUNT="tesi_ddellacasaventurelli01"
 TIME="01:00:00"
 GRES="gpu:1"
@@ -11,7 +10,7 @@ USE_IMMEDIATE=1
 IMMEDIATE_TIMEOUT=10
 PIN_NODE=1
 
-SRUN_ARGS="--partition=${PARTITION} --qos=${QOS} --account=${ACCOUNT} --time=${TIME} --gres=${GRES}"
+SRUN_ARGS="--partition=${PARTITION} --account=${ACCOUNT} --time=${TIME} --gres=${GRES}"
 
 if [ "${USE_IMMEDIATE}" -eq 1 ]; then
     SRUN_ARGS="${SRUN_ARGS} -Q --immediate=${IMMEDIATE_TIMEOUT}"
